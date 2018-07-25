1db9dabaa15a9b32cbc5873875753252635e6ae1
Icy Cloud <2226997440@qq.com>
2018-07-25 20:49:39 +0800

    Updated

--- Table.cmd
+++ Table.cmd
@@ -10,5 +10,5 @@ if NOT %ERRORLEVEL% == 0 pause
 exit
 :E_python
 echo No Python environment detected, please go visit https://www.python.org/downloads/windows/ to download and install a Python3 environment
-echo (Press any key to exist)
-pause >nul 2>nul
+echo Starting local CMD shell
+cmd
