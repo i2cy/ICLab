87dcaaacdcfefba6d5be81651eed8d225f207f6b
Icy Cloud <2226997440@qq.com>
2018-08-07 00:03:16 +0800

    updated 2018/8/7

--- Table.cmd
+++ Table.cmd
@@ -1,14 +1,17 @@
 @echo off
 title ICLab Table
-color 0b
+color 03
 py --version >nul 2>nul
 if NOT %ERRORLEVEL% == 0 goto E_python
 goto S_table
 :S_table
 py Table.py
-if NOT %ERRORLEVEL% == 0 pause
+if NOT %ERRORLEVEL% == 0 goto debug
 exit
 :E_python
 echo No Python environment detected, please go visit https://www.python.org/downloads/windows/ to download and install a Python3 environment
 echo Starting local CMD shell
 cmd
+:debug
+pause
+goto S_table
